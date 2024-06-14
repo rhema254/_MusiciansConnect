d16694c81e10dcb19921c1b8290d9a3203980f2c
rhema254 <karhem254@gmail.com>
2024-06-14 15:00:49 +0300

    deployment: static server updates

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -144,10 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', )
+    os.path.join(BASE_DIR, 'staticfiles', )
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
